50630aa6186720d7bf836b5ca7fd9080621c5ab2
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-07 15:31:08 +0200

    started to do routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:FormsComponent
+  },
+  {
+    path:'**',
+    component:FormsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
