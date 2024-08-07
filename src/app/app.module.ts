55e7dfc7939399d36df95b63e8eb22d5549cfddb
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-07 12:31:12 +0200

    added dome png files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr, ToastrModule } from 'ngx-toastr';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OfficeComponent } from './components/office/office.component';
+import { AnnuncioInfoComponent } from './components/annuncio-info/annuncio-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    FormsComponent,
+    HomeComponent,
+    NotFoundComponent,
+    OfficeComponent,
+    AnnuncioInfoComponent
   ],
   imports: [
     BrowserModule,
