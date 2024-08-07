24768c6cfa5d3425bfc906d0787f7c96800632d7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-07 15:34:01 +0200

    now doing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsComponent } from './components/forms/forms.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path:'',
     component:FormsComponent
+  },
+  {
+    path:'forms:id',
+    component:FormsComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
   },
   {
     path:'**',
