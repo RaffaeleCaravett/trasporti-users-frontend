2ddf365ba3be6664474d1649699a23f0a11dcbcd
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-09 11:59:00 +0200

    done validators on both forms, trasporti and azienda

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -24,16 +24,16 @@ this.signupForm=new FormGroup({
   regione:new FormControl('',Validators.required),
   indirizzo:new FormControl('',Validators.required),
   cap:new FormControl('',Validators.required),
-  email:new FormControl('',Validators.required),
+  email:new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]),
   password:new FormControl('',[Validators.required,Validators.minLength(6)]),
   type:new FormControl('',Validators.required)
 })
 this.trasportatoreForm= new FormGroup({
   nome:new FormControl('',Validators.required),
   cognome:new FormControl('',Validators.required),
-  eta:new FormControl('',Validators.required),
-  codiceFiscale:new FormControl('',[Validators.required]),
-  partitaIva:new FormControl('',[Validators.required]),
+  eta:new FormControl('',[Validators.required,Validators.min(18)]),
+  codiceFiscale:new FormControl('',[Validators.required,Validators.pattern('^[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z]{1}[0-9]{3}[A-Za-z]{1}$')]),
+  partitaIva:new FormControl('',[Validators.required,Validators.minLength(11)]),
   flottaMezzi:new FormControl('',Validators.required)
 })
 this.aziendaForm=  new FormGroup({
@@ -41,13 +41,13 @@ this.aziendaForm=  new FormGroup({
   fatturatoMedio:new FormControl('',Validators.required),
   numeroDipendenti:new FormControl('',Validators.required),
   settore:new FormControl('',[Validators.required]),
-  partitaIva:new FormControl('',[Validators.required])
+  partitaIva:new FormControl('',[Validators.required,Validators.minLength(11)])
 })
 }
 login(){
 
 }
 signup(){
-
+console.log(this.trasportatoreForm)
 }
 }
