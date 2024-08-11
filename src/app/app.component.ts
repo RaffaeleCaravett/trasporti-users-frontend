06d7c1e06da01b46a3ec88aabcf31ce48c7e07dd
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-11 17:26:14 +0200

    injected router in app compoennt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsService } from './shared/services/forms.service';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent implements OnInit{
 
   title = 'trasporti-users';
 
-constructor(private formsService:FormsService,private toastr:ToastrService){}
+constructor(private formsService:FormsService,private toastr:ToastrService,private router:Router){}
 
   ngOnInit(): void {
     let trasportatore = localStorage.getItem('TAccessToken')
@@ -28,15 +29,41 @@ this.formsService.verifyTrasportatoreRToken(trasportatoreR!).subscribe({
   next:(tokens:any)=>{
 localStorage.setItem('TrAccessToken',tokens.accessToken)
 localStorage.setItem('TrRefreshToken',tokens.refreshToken)
+this.formsService.setToken(tokens.accessToken)
+this.formsService.authenticateUser(true)
+
 },
   error:(error:any)=>{
     this.toastr.error("Non è stato possibile verificare la tua identità.")
-  }
+  },
+  complete:()=>{}
 })
         },
         complete:()=>{}
       })
     }
+if(azienda){
+  this.formsService.verifyAziendaToken(azienda).subscribe({
+    next:(a:any)=>{
+      localStorage.setItem('azienda',JSON.stringify(a))
+    },
+    error:(error:any)=>{
+this.formsService.verifyAziendaRToken(aziendaR!).subscribe({
+next:(tokens:any)=>{
+localStorage.setItem('AzAccessToken',tokens.accessToken)
+localStorage.setItem('AzRefreshToken',tokens.refreshToken)
+this.formsService.setToken(tokens.accessToken)
+this.formsService.authenticateUser(true)
 
+},
+error:(error:any)=>{
+this.toastr.error("Non è stato possibile verificare la tua identità.")
+},
+complete:()=>{}
+})
+    },
+    complete:()=>{}
+  })
+}
   }
 }
