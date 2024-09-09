58c266e2aea02e9bd43e1653ffb3d6c6f54a51bf
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-09 11:34:03 +0200

    now doing post recensioni

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,13 +7,21 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
   recensioneTForm!:FormGroup
   poli:string[]=['Positiva','Negativa']
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     console.log(data)
+  }
+
+  ngOnInit(): void {
+      console.log(this.data.role)
+      this.recensioneTForm = new FormGroup({
+        polo:new FormControl('',Validators.required),
+        message:new FormControl('')
+      })
   }
 
 inviaRecensione(){
--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -16,7 +16,25 @@ export class FormsService {
 getAzRecensioni(azId:number,stato:string,page:number,size:number,orderBy:string){
   return this.http.get(environment.API_URL+this.Az+this.recensione+this.paginated+`/${azId+'/'+stato+'?page='}`+page+'&size='+size+'&orderBy='+orderBy)
 }
-getSettori(){
-  return this.http.get(environment.API_URL+this.settori)
+postAzRecensione(recensione:any){
+  return this.http.post(environment.API_URL+this.Az+this.recensione,recensione)
+}
+putAzRecensioneById(azId:number,receId:number,recensione:any){
+  return this.http.put(environment.API_URL+this.Az+this.recensione+`/${azId}/${receId}`,recensione)
+}
+deleteAzRecensione(TId:number,receId:number){
+  return this.http.delete(environment.API_URL+this.Az+this.recensione+`/${TId}/${receId}`)
+}
+getTRecensioni(tId:number,stato:string,page:number,size:number,orderBy:string){
+  return this.http.get(environment.API_URL+this.T+this.recensione+this.paginated+`/${tId+'/'+stato+'?page='}`+page+'&size='+size+'&orderBy='+orderBy)
+}
+postTRecensione(recensione:any){
+  return this.http.post(environment.API_URL+this.T+this.recensione,recensione)
+}
+putTRecensioneById(tId:number,receId:number,recensione:any){
+  return this.http.put(environment.API_URL+this.T+this.recensione+`/${tId}/${receId}`,recensione)
+}
+deleteTRecensione(azId:number,receId:number){
+  return this.http.delete(environment.API_URL+this.T+this.recensione+`/${azId}/${receId}`)
 }
 }
