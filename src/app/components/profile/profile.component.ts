448524057a1173184c16fd6397215bcf61b1a919
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-09 11:54:29 +0200

    doing some more methods in forms service

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,44 +2,46 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { FormsService } from 'src/app/shared/services/forms.service';
 import { OfficeService } from 'src/app/shared/services/office.service';
 import { ProfileService } from 'src/app/shared/services/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit{
-
-  recensioneTForm!:FormGroup
-  poli:string[]=['Positiva','Negativa']
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private profileService:ProfileService, private toastr:ToastrService) {
-    console.log(data)
-  }
+export class ProfileComponent implements OnInit {
+  recensioneTForm!: FormGroup;
+  poli: string[] = ['Positiva', 'Negativa'];
+  user: any;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private profileService: ProfileService,
+    private toastr: ToastrService,
+    private formsService: FormsService
+  ) {}
 
   ngOnInit(): void {
-      console.log(this.data.role)
-      this.recensioneTForm = new FormGroup({
-        polo:new FormControl('',Validators.required),
-        message:new FormControl('')
-      })
+    this.user = this.formsService.getUser();
+    this.recensioneTForm = new FormGroup({
+      polo: new FormControl('', Validators.required),
+      message: new FormControl(''),
+    });
   }
 
-inviaRecensione(){
-  if(this.recensioneTForm.controls['polo'].valid){
-if(this.data.role=='Trasportatore'){
-this.profileService.postTRecensione(
-  {
-
+  inviaRecensione() {
+    if (this.recensioneTForm.controls['polo'].valid) {
+      if (this.data.role == 'Trasportatore') {
+        this.profileService.postTRecensione({
+          message: this.recensioneTForm.controls['message'].value || '',
+          polo: this.recensioneTForm.controls['polo'].value,
+          trasportatore_id: this.data.id,
+        });
+      } else {
+      }
+    } else {
+      this.toastr.show('Inserisci la valutazione');
+    }
   }
-)
-}else{
-
-}
-}else{
-this.toastr.show("Inserisci la valutazione")
-}
-}
 }
