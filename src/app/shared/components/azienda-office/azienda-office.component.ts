b9361036962801c6f026169e59b7675c3142e23a
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-15 15:24:23 +0200

    now developing azienda office

--- src/app/shared/components/azienda-office/azienda-office.component.ts
+++ src/app/shared/components/azienda-office/azienda-office.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-azienda-office',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./azienda-office.component.scss']
 })
 export class AziendaOfficeComponent {
+@Input() toDo:string=''
+@Input() azioni:string[]=[]
+
 
 }
