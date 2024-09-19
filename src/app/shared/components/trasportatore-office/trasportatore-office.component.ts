0ed0ccbcc8a339bacfa16e7bc505bef38a12e111
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-19 21:56:37 +0200

    console.logged(this.toDo) in toffice

--- src/app/shared/components/trasportatore-office/trasportatore-office.component.ts
+++ src/app/shared/components/trasportatore-office/trasportatore-office.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-trasportatore-office',
   templateUrl: './trasportatore-office.component.html',
   styleUrls: ['./trasportatore-office.component.scss']
 })
-export class TrasportatoreOfficeComponent {
+export class TrasportatoreOfficeComponent implements OnChanges{
 @Input() user:any
 @Input() toDo:string=''
 @Input() azioni:string[]=[]
+
+ngOnChanges():void{
+  console.log(this.toDo)
+}
 }
