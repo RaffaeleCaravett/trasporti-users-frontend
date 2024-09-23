64f53ccb3e09ea3ce7ccb5d363d7279d087dc6fb
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-23 09:07:48 +0200

    implemented constructor and OnInit in show annuncio

--- src/app/shared/components/show-annuncio/show-annuncio.component.ts
+++ src/app/shared/components/show-annuncio/show-annuncio.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-show-annuncio',
   templateUrl: './show-annuncio.component.html',
   styleUrls: ['./show-annuncio.component.scss']
 })
-export class ShowAnnuncioComponent {
+export class ShowAnnuncioComponent implements OnInit{
 
+
+  constructor(){}
+
+ngOnInit(): void {
+
+}
 }
