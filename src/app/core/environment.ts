70526a9bfa58fa7ffb1602c896a25db5f6d1e8bf
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-28 20:45:06 +0100

    added two variables to environment

--- src/app/core/environment.ts
+++ src/app/core/environment.ts
@@ -1,4 +1,5 @@
 export const environment ={
   API_URL : 'http://localhost:3031',
-  WEBSOCKET_API_URL:'https://localhost:3031/ws'
+  WEBSOCKET_API_URL:'ws://192.168.1.60:3032',
+  NETLIFY_WEBSOCKET_API_URL:'wss://192.168.1.60:3032',
 }
