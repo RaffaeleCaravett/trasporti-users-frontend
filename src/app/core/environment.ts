f490011af15e0bed18616e97805f6ac1d3339297
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-29 21:29:13 +0100

    trying to debug backend

--- src/app/core/environment.ts
+++ src/app/core/environment.ts
@@ -1,7 +1,7 @@
 export const environment ={
   API_URL : 'http://localhost:3031',
   WEBSOCKET_API_URL:'ws://192.168.1.60:3032',
-  NETLIFY_WEBSOCKET_API_URL:'wss://192.168.1.254:3032',
+  NETLIFY_WEBSOCKET_API_URL:'wss://192.168.1.60:3032',
   NETLIFY_LOCALHOST_WEBSOCKET_API_URL:'wss://localhost:3032',
   NETLIFY_EMPTY_API_URL:'http://192.168.1.60:3032'
 }
