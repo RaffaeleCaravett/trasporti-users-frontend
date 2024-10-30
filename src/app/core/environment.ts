45516676b7885be0422869c995cc4a30cb411a48
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-30 20:23:27 +0100

    oh shit

--- src/app/core/environment.ts
+++ src/app/core/environment.ts
@@ -2,7 +2,7 @@ export const environment ={
   API_URL : 'http://localhost:3031',
   GH_PAGES_API_URL: 'https://raffaelecaravett.github.io/Trasporti-Backend/',
   WEBSOCKET_API_URL:'ws://192.168.1.60:3032',
-  NETLIFY_WEBSOCKET_API_URL:'wss://192.168.1.60:55401',
+  NETLIFY_WEBSOCKET_API_URL:'https://192.168.1.60:3032',
   NETLIFY_LOCALHOST_WEBSOCKET_API_URL:'wss://localhost:3032',
   NETLIFY_EMPTY_API_URL:'http://192.168.1.60:3032',
 }
