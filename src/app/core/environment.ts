b12874c16ec2fe669e78d48ec54530b8c8b534af
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-30 20:15:55 +0100

    changed variable value

--- src/app/core/environment.ts
+++ src/app/core/environment.ts
@@ -1,5 +1,5 @@
 export const environment ={
-  API_URL : 'https://raffaelecaravett.github.io/Trasporti-Backend',
+  API_URL : 'http://localhost:3031',
   GH_PAGES_API_URL: 'https://raffaelecaravett.github.io/Trasporti-Backend/',
   WEBSOCKET_API_URL:'ws://192.168.1.60:3032',
   NETLIFY_WEBSOCKET_API_URL:'wss://192.168.1.60:55401',
