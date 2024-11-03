ef5bf23ce4cf70118ef176b6c1bd965ea4c560c0
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-11-03 21:38:56 +0100

    putting my hands on it

--- src/app/shared/components/user-info/user-info.component.ts
+++ src/app/shared/components/user-info/user-info.component.ts
@@ -3,36 +3,50 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.scss']
+  styleUrls: ['./user-info.component.scss'],
 })
-export class UserInfoComponent implements OnInit{
-@Input() user:any
-@Input() isTrasportatore!:boolean
-poloRecensioni:string=''
-recensioniNegative:any[]=[]
-recensioniPositive:any[]=[]
-recensioniClass:string =''
-ngOnInit(): void {
-    this.recensioniNegative= this.user.recensioniRicevute.filter((r:any)=>{return r.poloRecensione=='negativa'})
-    this.recensioniPositive= this.user.recensioniRicevute.filter((r:any)=>{return r.poloRecensione=='positiva'})
-  this.recensioniNegative>this.recensioniPositive?this.poloRecensioni="Gli utenti ti hanno reputato negativamente":this.recensioniNegative<this.recensioniPositive?this.poloRecensioni="Gli utenti ti hanno reputato positivamente":this.recensioniNegative==this.recensioniPositive?this.poloRecensioni="Il giudizio non pende ad nessuna parte.":"";
-switch(this.poloRecensioni){
-  case "":{
-
+export class UserInfoComponent implements OnInit {
+  @Input() user: any;
+  @Input() isTrasportatore!: boolean;
+  poloRecensioni: string = '';
+  recensioniNegative: any[] = [];
+  recensioniPositive: any[] = [];
+  recensioniClass: string = '';
+  ngOnInit(): void {
+    this.recensioniNegative = this.user.recensioniRicevute.filter((r: any) => {
+      return r.poloRecensione == 'negativa';
+    });
+    this.recensioniPositive = this.user.recensioniRicevute.filter((r: any) => {
+      return r.poloRecensione == 'positiva';
+    });
+    this.recensioniNegative > this.recensioniPositive
+      ? (this.poloRecensioni = 'Ti valutano negativamente, mannaggia!')
+      : this.recensioniNegative < this.recensioniPositive
+      ? (this.poloRecensioni = 'Ti valutano positivamente, complimenti!')
+      : (this.recensioniNegative == this.recensioniPositive)||(this.recensioniNegative.length==0&&this.recensioniPositive.length==0)
+      ? (this.poloRecensioni = 'Il giudizio non pende da nessuna parte.')
+      : '';
+    switch (this.poloRecensioni) {
+      case 'Ti valutano negativamente, mannaggia!':
+        {
+          this.recensioniClass = 'text-danger';
+        }
+        break;
+      case 'Ti valutano positivamente, complimenti!':
+        {
+          this.recensioniClass = 'text-success';
+        }
+        break;
+      case 'Il giudizio non pende da nessuna parte.':
+        {
+          this.recensioniClass = 'text-success';
+        }
+        break;
+      default:
+        {
+          this.recensioniClass=''
+        }
+        break;
+    }
   }
-  break;
-  case "":{
-
-  }
-  break;
-  case "":{
-
-  }
-  break;
-  default:{
-
-  }
-  break;
-}
-}
 }
