36912194bcb3d03788a8e363d9492fdab509dd25
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-11-09 21:09:59 +0100

    enoughe for today

--- src/app/components/show-spedizione/show-spedizione.component.ts
+++ src/app/components/show-spedizione/show-spedizione.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-show-spedizione',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-spedizione.component.scss']
 })
 export class ShowSpedizioneComponent {
+  spedizione:any
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
 
+  ) {
+    console.log(data)
+  }
+
+  ngOnInit(): void {
+  this.spedizione=this.data
+  }
 }
