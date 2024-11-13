ae10d1c73f422b17fa54ae08ebb2c87404473e74
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-11-13 19:02:05 +0100

    succesfuly implemented modify t

--- src/app/shared/services/office.service.ts
+++ src/app/shared/services/office.service.ts
@@ -71,7 +71,7 @@ putAziendaById(aziendaDTO:{},aziendaId?:number){
   return this.httpClient.put(environment.API_URL+this.azienda+`${aziendaId!=null&&aziendaId!=undefined&&aziendaId!=0&&aziendaId?'/'+aziendaId:'/me'}`,aziendaDTO)
 }
 putTrasportatoreById(trasportatoreDTO:{},trasportatoreId?:number){
-  return this.httpClient.put(environment.API_URL+this.azienda+`${trasportatoreId!=null&&trasportatoreId!=undefined&&trasportatoreId!=0&&trasportatoreId?'/'+trasportatoreId:'/me'}`,trasportatoreDTO)
+  return this.httpClient.put(environment.API_URL+this.trasportatore+`${trasportatoreId!=null&&trasportatoreId!=undefined&&trasportatoreId!=0&&trasportatoreId?'/'+trasportatoreId:'/me'}`,trasportatoreDTO)
 }
 deleteProfile(){
 
