f54ef5ad92723fa9ef6cb79acf2e3ca929379077
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-11-18 10:26:25 +0100

    injected chat service into toffice

--- src/app/components/home/chat/chat.component.ts
+++ src/app/components/home/chat/chat.component.ts
@@ -1,9 +1,6 @@
-import {
-  AfterViewChecked,
-  Component,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { AfterViewChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { delay } from 'rxjs';
 import { ChatService } from 'src/app/shared/services/chat.service';
 import { FormsService } from 'src/app/shared/services/forms.service';
@@ -19,9 +16,13 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
   chats: any = null;
   isLoading: boolean = false;
   isThereaSelectedChat: boolean = false;
+  sendMessageForm: FormGroup = new FormGroup({
+    testo: new FormControl('', [Validators.required, Validators.minLength(1)]),
+  });
   constructor(
     private formsService: FormsService,
-    private chatService: ChatService
+    private chatService: ChatService,
+    private toastr: ToastrService
   ) {
     this.user = this.formsService.getUser();
     this.selectedChat = this.chatService.getSelectedChat();
@@ -94,6 +95,7 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
   }
   assignChat(chat: any) {
     this.selectedChat = chat;
+    this.chatService.setSelectedChat(chat);
     this.isThereaSelectedChat = true;
     localStorage.setItem('selectedChatId', this.selectedChat.id);
     this.scrollToBottom();
@@ -106,6 +108,38 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
     if (chatContainer != null) {
       chatContainer.style.scrollBehavior = 'smooth';
       chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }
+
+  sendMessage() {
+    if (this.sendMessageForm.valid) {
+      let message: any = {
+        testo: this.sendMessageForm.controls['testo'].value,
+        chat_id: this.selectedChat.id,
+        sender_id: this.user.id,
+        receiver_id:
+          this.user.role == 'Azienda'
+            ? this.selectedChat.trasportatore.id
+            : this.selectedChat.azienda.id,
+        receiverType:
+          this.user.role == 'Trasportatore' ? 'Azienda' : 'Trasportatore',
+        senderType:
+          this.user.role == 'Trasportatore' ? 'Trasportatore' : 'Azienda',
+        room: this.selectedChat.id,
+      };
+      this.chatService.sendMessage(message).subscribe({
+        next:(mess:any)=>{
+          this.selectedChat.messaggiList.push(mess)
+          let chatContainer = document.getElementsByClassName(
+            'chats-container'
+          )[1] as HTMLDivElement;
+            chatContainer.style.scrollBehavior = 'smooth';
+            chatContainer.scrollTop = chatContainer.scrollHeight;
+        this.sendMessageForm.reset()
+          }
+      });
+    } else {
+      this.toastr.error('Scrivi qualcosa.');
     }
   }
 }
